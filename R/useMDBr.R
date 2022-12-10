1e3bc1a27e4897722c53496efcd30edd6107cd2c
Wardiam <wardiam@gmail.com>
2022-12-11 00:43:48 +0100

    Actualizaxcion mdb5

--- R/useMDBr.R
+++ R/useMDBr.R
@@ -4,7 +4,7 @@ useMDBr <- function() {
     version = "0.1",
     package = "MDBr",
     src = "assets",
-    stylesheet = c("css/bootstrap.min.css", "css/mdb.min.css"),
-    script = c("js/popper.min.js", "js/bootstrap.min.js", "js/mdb.min.js")
+    stylesheet = c("css/mdb.min.css"),
+    script = c("js/mdb.min.js")
   )
 }
